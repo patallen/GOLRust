88df0baed879a7b96becb563d6cbe1697b86fbb8
Patrick Allen <prallen90@gmail.com>
2017-06-04 20:13:55 -0400

    Small constant fixes

--- src/main.rs
+++ src/main.rs
@@ -6,31 +6,34 @@ mod board;
 mod game;
 
 use game::GameScene;
+use engine::engine::{Engine, EngineMeta};
 
 pub const SPEED: usize = 100;
+pub const TITLE: &'static str = "Game of Life";
+
 
 #[cfg(target_os = "emscripten")]
 mod consts {
-    pub const WIDTH: u32 = 60;
-    pub const HEIGHT: u32 = 40;
-    pub const SCALE: u32 = 12;
+    pub const WIDTH: usize = 60;
+    pub const HEIGHT: usize = 40;
+    pub const SCALE: usize = 12;
 }
 
 
 #[cfg(not(target_os = "emscripten"))]
 mod consts {
-    pub const WIDTH: u32 = 160;
-    pub const HEIGHT: u32 = 100;
-    pub const SCALE: u32 = 8;
+    pub const WIDTH: usize = 160;
+    pub const HEIGHT: usize = 100;
+    pub const SCALE: usize = 8;
 }
 
 use consts::*;
 
 fn main() {
-    let context = sdl2::init().expect("Trouple initializing sdl2");
-    let meta = engine::engine::EngineMeta::new("GOL", WIDTH, HEIGHT, SCALE);
-    let mut game = engine::engine::Engine::new(context, meta);
-    game.add_scene(Box::new(GameScene::new(WIDTH as usize, HEIGHT as usize, SCALE as usize, SPEED)));
+    let context = sdl2::init().expect("Trouple initializing sdl2.");
+    let meta = EngineMeta::new(TITLE, WIDTH as u32, HEIGHT as u32, SCALE as u32);
+    let mut game = Engine::new(context, meta);
+    game.add_scene(Box::new(GameScene::new(WIDTH, HEIGHT, SCALE, SPEED)));
 
 
     let mut main_loop = || {
